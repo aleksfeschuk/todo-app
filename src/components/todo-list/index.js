adb6f6325c3922ca9ac0be9e73e24bb3e87167fc
aleksfeschuk <https://github.com/aleksfeschuk>
2024-03-06 00:53:31 +0100

    separate file component

---
+++ src/components/todo-list/index.js
@@ -0,0 +1,5 @@
+import TodoList from "./todo-list";
+
+export default TodoList;
+
+//refactors
--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
 import React from 'react';
 import  ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import TodoList from './components/todo-list';
-import SearchPanel from './components/search-panel';
-import ItemStatusFilter from './components/item-status-filter';
 
-
-import './index.css';
-
-const App = () => {
-
-  const todoData = [
-    {label: 'Drink Coffe', important: false, id: 1 },
-    {label: 'Make Awesome App', important: true, id: 2 },
-    {label: 'Have a lunch', important: false, id: 3 },
-  ];
-
-
-  return (
-    <div className="todo-app">
-        <AppHeader toDo={1} done={3}/>
-        <div className="top-panel d-flex">
-          <SearchPanel />
-          <ItemStatusFilter />
-        </div>
-        <TodoList todos={todoData}/>
-    </div>
-  );
-};
+//Refactoring codes
+import App from './components/app';
 
 
 
