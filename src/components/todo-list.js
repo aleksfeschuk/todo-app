aa481fcad2b8152ac716eaa1565f363c6443efcd
aleksfeschuk <https://github.com/aleksfeschuk>
2024-03-05 20:50:24 +0100

    props

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -4,10 +4,13 @@ import TodoListItem from './todo-list-item';
 const TodoList = () => {
     return (
       <ul>
-        <li><TodoListItem /></li>
-        <li><TodoListItem /></li>
+        <li><TodoListItem 
+                label="Drink Coffe" /></li>
+        <li><TodoListItem 
+                label="Build React App"
+                important /></li>
       </ul>
-    )
+    );
 };
 
 export default TodoList;
