9b39e3d52604797ba60e1030a30fe364c23f88c7
aleksfeschuk <https://github.com/aleksfeschuk>
2024-03-31 21:36:51 +0200

    setState use methods splice

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
 
@@ -11,26 +11,47 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-  const todoData = [
-    {label: 'Drink Coffe', important: false, id: 1 },
-    {label: 'Make Awesome App', important: true, id: 2 },
-    {label: 'Have a lunch', important: false, id: 3 },
-  ];
+  state = {
+    todoData: [
+      {label: 'Drink Coffe', important: false, id: 1 },
+      {label: 'Make Awesome App', important: true, id: 2 },
+      {label: 'Have a lunch', important: false, id: 3 }
+    ]
+  };
+
+  deleteItem = (id) => {
+      this.setState(({ todoData }) => {
+        const idx = todoData.findIndex((el) => el.id === id);
 
 
-  return (
-    <div className="todo-app">
-        <AppHeader toDo={1} done={3}/>
-        <div className="top-panel d-flex">
-          <SearchPanel />
-          <ItemStatusFilter />
-        </div>
-        <TodoList todos={todoData}/>
-    </div>
-  );
+        const newArray = [
+          ...todoData.slice(0, idx), 
+          ...todoData.slice(idx + 1)
+        ];
+
+
+        return {
+          todoData: newArray
+        }
+
+      })
+
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+          <AppHeader toDo={1} done={3}/>
+          <div className="top-panel d-flex">
+            <SearchPanel />
+            <ItemStatusFilter />
+          </div>
+          <TodoList 
+          todos={this.state.todoData}
+          onDeleted={ this.deleteItem }/>
+      </div>
+    );
+  }
 };
-
-
-export default App;
