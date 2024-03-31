9224bf16dc5ef3205b2f661f9abd7e5a7ab43543
aleksfeschuk <https://github.com/aleksfeschuk>
2024-03-31 20:31:22 +0200

    state comonents-reacts

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,8 +3,27 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+
+    state = {
+        done: false
+    };
+    
+    onLabelClick = () => {
+        this.setState({
+            done: true
+        });
+    };
+    
+
+
     render() {
         const { label, important = false } = this.props;
+        const { done } = this.state;
+
+        let classNames = 'todo-list-item'
+        if (done) {
+            classNames += ' done';
+        }
 
             const style = {
                 color: important ? 'tomato' : 'black',
@@ -12,10 +31,11 @@ export default class TodoListItem extends Component {
             };
         
             return (
-                <span className="todo-list-item">
+                <span className={classNames}>
                     <span  
                         className="todo-list-item-label" 
-                        style={style}>
+                        style={style}
+                        onClick = { this.onLabelClick }>
                         { label }
                     </span>
                 
